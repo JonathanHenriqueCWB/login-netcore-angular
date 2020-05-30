77ed81e00008979eae54f038cbb8eebb2d049c48
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-05-30 09:57:37 -0300

    Criando a classe de contexto

---
+++ Cadastro.Repository/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using Cadastro.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro.Repository.Data
+{
+    class Context : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
