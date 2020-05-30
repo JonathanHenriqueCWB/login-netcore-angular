1f6d03b2897904e0ae9e383b12c249d4e54795c1
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-05-30 09:30:35 -0300

    Criando as interfaces de contrato

---
+++ Cadastro.Domain/Contratos/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cadastro.Domain.Contratos
+{
+    public interface IBaseRepository<TEntity> :IDisposable where TEntity : class
+    {
+        IEnumerable<TEntity> Listar();
+        void Cadastrar(TEntity entity);
+        void Atualizar(TEntity entity);
+        void Remover(TEntity entity);
+    }
+}
---
+++ Cadastro.Domain/Contratos/IUsuarioRepository.cs
@@ -0,0 +1,8 @@
+﻿using Cadastro.Domain.Entidades;
+
+namespace Cadastro.Domain.Contratos
+{
+    interface IUsuarioRepository : IBaseRepository<Usuario>
+    {
+    }
+}
