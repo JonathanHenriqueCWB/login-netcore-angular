975e493d49bde80d94e908219a58fcd2c9736630
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-05-30 09:22:40 -0300

    Criando as entidades

---
+++ Cadastro.Domain/Entidades/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Cadastro.Domain.Entidades
+{
+    class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
