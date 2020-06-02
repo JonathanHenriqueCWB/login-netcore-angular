ef8eeccdcab2d24f50eda9aae80e005ba5162356
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-06-02 09:03:03 -0300

    Criando as entidades (Angular)

--- Cadastro.Web/ClientApp/src/app/usuario/login/login.usuario.component.ts
+++ Cadastro.Web/ClientApp/src/app/usuario/login/login.usuario.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../model/usuario';
 
 @Component({
   selector: 'app-login-usuario',
   templateUrl: './login.usuario.component.html',
   styleUrls: ['./login.usuario.component.css']
 })
-export class LoginUsuarioComponente {
+export class LoginUsuarioComponente implements OnInit {
+  
+  public usuario;
 
-  constructor() {
+  ngOnInit(): void {
+    this.usuario = new Usuario();
   }
 
   public logar() {
-    console.log("Logando com usuario...")
+    console.log(this.usuario.nome, this.usuario.sobrenome, this.usuario.email, this.usuario.senha);
   }
 }
